c838672c3595af8b057173b0ed9e119e0dd302b4
전유정 <jyjyyj0705@duksung.ac.kr>
2026-03-10 10:17:10 +0900

    first commit

---
+++ src/practice/ch01/Main.java
@@ -0,0 +1,7 @@
+package practice.ch01;
+
+public class Main {
+    public void main(String[] args)
+        Book book = new Book("Java Programming");
+        System.out.println("Book name: " + book.getName())
+}
