e33cda75b0ebf24970ec2b54def12cb6a8b800ff
전유정 <jyjyyj0705@duksung.ac.kr>
2026-05-20 18:18:24 +0900

    17장 과제 완료

--- src/hw/ch17/Main.java
+++ src/hw/ch17/Main.java
@@ -2,6 +2,7 @@ package hw.ch17;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("학번: 20230887, 이름: 전유정");
         NumberGenerator generator = new EvenNumberGenerator(0, 30 );
         Observer observer1 = new DigitObserver();
         Observer observer2 = new GraphObserver();
