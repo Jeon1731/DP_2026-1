18b7352286a54d404f37b7b70293c944839901e7
전유정 <jyjyyj0705@duksung.ac.kr>
2026-06-05 15:59:33 +0900

    0521 수업 완료

---
+++ src/practice/ch20/Main.java
@@ -0,0 +1,30 @@
+package practice.ch20;
+
+public class Main {
+    public static void main(String[] args) {
+        BigChar char1 = new BigChar('1');
+        char1.print();
+
+        BigChar char2 = new BigChar('1');
+        char2.print();
+
+        // BigCharFactory를 이용
+        BigCharFactory factory = BigCharFactory.getInstance();
+
+        BigChar char3 = factory.getBigChar('1');
+        char3.print();
+
+        BigChar char4 = factory.getBigChar('1');
+        char4.print();
+
+        if (char3 == char4) {
+            System.out.println("char3와 char4는 같은 BigChar 인스턴스입니다.");
+        } else {
+            System.out.println("char3와 char4는 다른 BigChar 인스턴스입니다.");
+        }
+
+        // BigString을 이용
+        BigString bigString = new BigString("1212123");
+        bigString.print();
+    }
+}
